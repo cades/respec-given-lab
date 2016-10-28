d4dd88dea2732cff4dd889a878313a794bb444fe
cades kao <cadeskao@gmail.com>
2016-10-28 18:09:16 +0800

    (feat) test suite can be inlined via config

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var respecGivenCore = require('respec-given-core')
 function noop(){}
 
-module.exports = function(lab) {
+module.exports = function(lab, opts) {
+  opts = opts || {}
+
+  // decide implementation
+  var suiteType = opts.inlineSuite ? 'inline' : 'normal'
+  var createLabel = {
+    inline: function (label) { return nameStack.join(' ') + ' ' + label },
+    normal: function (label) { return label }
+  }[suiteType]
+
+  var createSuite = {
+    inline: function(title, options, defineSuite) {
+      nameStack.push(title)
+      defineSuite()
+      nameStack.pop()
+    },
+    normal: function(title, options, defineSuite) {
+      lab.experiment(title, options, defineSuite)
+    }
+  }[suiteType]
 
   var core = respecGivenCore.createCore({
     rootSuite: null,
@@ -10,9 +29,11 @@ module.exports = function(lab) {
       lab.experiment.skip(title)
     },
     addTestCallback: function(_, label, fn) {
-      lab.test(label, fn)
+      lab.test(createLabel(label), fn)
     }
   })
+
+  var nameStack = []
   var context = {}
 
   // traditional describe/it BDD interface
@@ -21,7 +42,7 @@ module.exports = function(lab) {
     const options = arguments.length === 3 ? arguments[1] : {}
     const fn = arguments.length === 3 ? arguments[2] : arguments[1],
           wrappedFn = core.describe.bind(null, title, fn)
-    lab.experiment(title, options, wrappedFn)
+    createSuite(title, options, wrappedFn)
   }
 
   context.it = context.specify = function (title) {
